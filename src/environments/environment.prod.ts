21dc7cb232376bc78def51fc37c5da2dc6667498
ngozi-ekekwe <rose.ekekwe@gmail.com>
2019-10-06 16:18:54 +0100

    add firebase

--- src/environments/environment.prod.ts
+++ src/environments/environment.prod.ts
@@ -1,3 +1,13 @@
 export const environment = {
-  production: true
+  production: true,
+  firebase: {
+    apiKey: "AIzaSyDofegEPpedNNb0O_Er11E41KVcNnSlazY",
+    authDomain: "oshop-13458.firebaseapp.com",
+    databaseURL: "https://oshop-13458.firebaseio.com",
+    projectId: "oshop-13458",
+    storageBucket: "",
+    messagingSenderId: "925487398507",
+    appId: "1:925487398507:web:9fea3119ead321b971fb2e",
+    measurementId: "G-F20DGWZYY0"
+  }
 };
--- src/environments/environment.ts
+++ src/environments/environment.ts
@@ -3,7 +3,17 @@
 // The list of file replacements can be found in `angular.json`.
 
 export const environment = {
-  production: false
+  production: false,
+  firebase: {
+    apiKey: "AIzaSyDofegEPpedNNb0O_Er11E41KVcNnSlazY",
+    authDomain: "oshop-13458.firebaseapp.com",
+    databaseURL: "https://oshop-13458.firebaseio.com",
+    projectId: "oshop-13458",
+    storageBucket: "",
+    messagingSenderId: "925487398507",
+    appId: "1:925487398507:web:9fea3119ead321b971fb2e",
+    measurementId: "G-F20DGWZYY0"
+  }
 };
 
 /*
